4bf37357d71986592384ad89c4e6acb8a713c3e9
Ahmed Mahmoud <92031163+ahmed141631@users.noreply.github.com>
2023-04-18 19:48:58 +0200

    Update main.cc

--- Ds_Implementation/LinkedList/main.cc
+++ Ds_Implementation/LinkedList/main.cc
@@ -7,13 +7,76 @@ using namespace std;
 
 int main()
 {
-    Node *node = new Node(1,2);
-   LinkedList *list = new LinkedList(node);
-    list->append(new Node(3,4));
-    list->append(new Node(5,6));
-    list->print();
-    list->insert(2,new Node(7,8));
-    list->print();
+    LinkedList s;
+  int option;
+  int key1, k1, data1;
+  do {
+    cout << "\nWhat operation do you want to perform? Select Option number. Enter 0 to exit." << endl;
+    cout << "1. appendNode()" << endl;
+    cout << "2. prependNode()" << endl;
+    cout << "3. insertNodeAfter()" << endl;
+    cout << "4. deleteNodeByKey()" << endl;
+    cout << "5. print()" << endl;
+    cout << "7. Clear Screen" << endl << endl;
+
+    cin >> option;
+    Node * n1 = new Node();
+    //Node n1;
+
+    switch (option) {
+    case 0:
+      break;
+    case 1:
+      cout << "Append Node Operation \nEnter key & data of the Node to be Appended" << endl;
+      cin >> key1;
+      cin >> data1;
+      n1 -> setKey( key1);
+      n1 ->setValue( data1);
+      s.append(n1);
+      //cout<<n1.key<<" = "<<n1.data<<endl;
+      break;
+
+    case 2:
+      cout << "Prepend Node Operation \nEnter key & data of the Node to be Prepended" << endl;
+      cin >> key1;
+      cin >> data1;
+      n1 -> setKey( key1);
+      n1 -> setValue( data1);
+      s.prepend(n1);
+      break;
+
+    case 3:
+      cout << "Insert Node After Operation \nEnter key of existing Node after which you want to Insert this New node: " << endl;
+      cin >> k1;
+      cout << "Enter key & data of the New Node first: " << endl;
+      cin >> key1;
+      cin >> data1;
+      n1 -> setKey( key1);
+      n1 ->setValue( data1);
+
+      s.insertNodeAfter(k1, n1);
+      break;
+
+    case 4:
+
+      cout << "Delete Node By Key Operation - \nEnter key of the Node to be deleted: " << endl;
+      cin >> k1;
+      s.remove(k1);
+
+      break;
+    
+    case 5:
+      s.print();
+
+      break;
+    case 7:
+      system("cls");
+      break;
+    default:
+      cout << "Enter Proper Option number " << endl;
+    }
+
+  } while (option != 0);
     
     return 0;
 }
